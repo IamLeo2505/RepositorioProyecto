a84ceaf9066bd36460fd480e1b8febabf8f3c6a5
LeandroMatus <matusleandro12@gmail.com>
2024-10-27 20:22:53 -0600

    Vista Categoría y Marca conectadas
    Revisar Funcionalidad*

---
+++ D-ALTURA/View/wwwroot/js/registrarcategoria.js
@@ -0,0 +1,40 @@
+document.addEventListener('DOMContentLoaded', function () {
+    document.getElementById('form-categoria').addEventListener('submit', function (event) {
+        event.preventDefault();
+
+        const descripcion = document.getElementById('descripcion').value;
+        
+
+        // Validar si hay algún campo vacío
+        if (!descripcion) {
+            alert('Por favor, agregue una categoría.');
+            return;
+        }
+
+        // Realizar la solicitud fetch
+        fetch('http://localhost:5000/api/Categoría/GuardarCambios', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({
+                descripcion: descripcion
+            }),
+        })
+        .then(response => {
+            if (!response.ok) {
+                throw new Error('Error en la solicitud');
+            }
+            return response.json();
+        })
+        .then(data => {
+            alert('Los datos se han guardado exitosamente.');
+            console.log(data);
+        })
+        .catch(error => {
+            alert('Error: ' + error.message);
+            console.error('Error:', error);
+        });
+    });
+});
+
